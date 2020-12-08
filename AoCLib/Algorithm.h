ee4a375c3682a9874be71c0c8ff0f12de7bd19ad
CristianTudor89 <48018200+CristianTudor89@users.noreply.github.com>
2020-12-08 11:04:12 +0200

    Add files via upload

---
+++ AoCLib/Algorithm.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Algorithm
+{
+template <class Container, class T>
+bool Contains(const Container & aContainer, const T & aValue)
+{
+  auto beginIt = begin(aContainer);
+  auto endIt   = end(aContainer);
+
+  return find(beginIt, endIt, aValue) != endIt;
+}
+};  // namespace Algorithm
