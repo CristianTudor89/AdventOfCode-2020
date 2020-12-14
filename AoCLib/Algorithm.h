197240a487890da0bbe54c6631af2b7698235912
CristianTudor89 <48018200+CristianTudor89@users.noreply.github.com>
2020-12-14 14:28:41 +0200

    Add files via upload

--- AoCLib/Algorithm.h
+++ AoCLib/Algorithm.h
@@ -12,4 +12,18 @@ bool Contains(const Container & aContainer, const T & aValue)
 
   return find(beginIt, endIt, aValue) != endIt;
 }
+
+template <class T>
+void InsertOrUpdate(vector<pair<T, T>> & aContainer, const T & aKey, const T & aValue)
+{
+  auto it = std::find_if(aContainer.begin(), aContainer.end(), [&aKey](const auto & aKeyValuePair) {
+    return aKeyValuePair.first == aKey;
+  });
+
+  if (it != aContainer.end())
+    it->second = aValue;
+  else
+    aContainer.push_back({ aKey, aValue });
+}
+
 };  // namespace Algorithm
